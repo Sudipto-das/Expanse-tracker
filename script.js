4f6fbb4c8598a8384f65bc8ded30da52a1ea26d9
Sudipto das <sudiptodas877@gmail.com>
2026-01-02 17:01:13 +0530

    edit option added

--- script.js
+++ script.js
@@ -5,51 +5,68 @@ const list = document.getElementById("expense-list");
 const totalEl = document.getElementById("total");
 
 let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+let editIndex=null;
 
 function saveToLocalStorage() {
-  localStorage.setItem("expenses", JSON.stringify(expenses));
+    localStorage.setItem("expenses", JSON.stringify(expenses));
 }
 
 function calculateTotal() {
-  const total = expenses.reduce((sum, exp) => sum + exp.amount, 0);
-  totalEl.textContent = total;
+    const total = expenses.reduce((sum, exp) => sum + exp.amount, 0);
+    totalEl.textContent = total;
 }
 
 function renderExpenses() {
-  list.innerHTML = "";
+    list.innerHTML = "";
 
-  expenses.forEach((expense, index) => {
-    const li = document.createElement("li");
-    li.innerHTML = `
+    expenses.forEach((expense, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
       ${expense.title} - ₹${expense.amount}
-      <button class="delete" onclick="deleteExpense(${index})">X</button>
-    `;
-    list.appendChild(li);
-  });
+      <div>
 
-  calculateTotal();
+      <button class = "edit" onClick="editExpense(${index})">Edit</button>
+      <button class="delete" onclick="deleteExpense(${index})">X</button>
+      </div>
+      
+    `;
+        list.appendChild(li);
+    });
+
+    calculateTotal();
+}
+function editExpense(index) {
+    titleInput.value = expenses[index].title;
+    amountInput.value = expenses[index].amount;
+    editIndex = index;
 }
 
 function deleteExpense(index) {
-  expenses.splice(index, 1);
-  saveToLocalStorage();
-  renderExpenses();
+    expenses.splice(index, 1);
+    saveToLocalStorage();
+    renderExpenses();
 }
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  const expense = {
-    title: titleInput.value,
-    amount: Number(amountInput.value),
-  };
+    const expense = {
+        title: titleInput.value,
+        amount: Number(amountInput.value),
+    };
+    if(editIndex === null){
+        expenses.push(expense);
+    } else {
+        expenses[editIndex] = expense;
+        editIndex =null;
+    }
 
-  expenses.push(expense);
-  saveToLocalStorage();
-  renderExpenses();
+    
+    saveToLocalStorage();
+    renderExpenses();
 
-  titleInput.value = "";
-  amountInput.value = "";
+    titleInput.value = "";
+    amountInput.value = "";
 });
 
 // Initial load
